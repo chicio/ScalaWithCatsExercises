3e9b978e6130ad2cf71b4cec9375417f21966af6
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2019-06-27 17:28:53 +0200

    More exercises :rocket:

--- src/main/scala/it/chicio/scalawithcats/chapter2/Boolean.scala
+++ src/main/scala/it/chicio/scalawithcats/chapter2/Boolean.scala
@@ -10,3 +10,50 @@ object Monoid {
   def apply[A](implicit monoid: Monoid[A]) = monoid
 }
 
+object BooleanInstances {
+  implicit val and = new Monoid[Boolean] {
+    override def empty: Boolean = true
+    override def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  implicit val or = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+
+  implicit val xor = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean = x ^ y
+  }
+
+  implicit val xnor = new Monoid[Boolean] {
+    override def empty: Boolean = true
+    override def combine(x: Boolean, y: Boolean): Boolean = x == y
+  }
+}
+
+object Boolean extends App {
+  //AND
+  println(Monoid(BooleanInstances.and).combine(true, true))
+  println(Monoid(BooleanInstances.and).combine(true, false))
+  println(Monoid(BooleanInstances.and).combine(false, true))
+  println(Monoid(BooleanInstances.and).combine(false, false))
+
+  //OR
+  println(Monoid(BooleanInstances.or).combine(true, true))
+  println(Monoid(BooleanInstances.or).combine(true, false))
+  println(Monoid(BooleanInstances.or).combine(false, true))
+  println(Monoid(BooleanInstances.or).combine(false, false))
+
+  //XOR
+  println(Monoid(BooleanInstances.xor).combine(true, true))
+  println(Monoid(BooleanInstances.xor).combine(true, false))
+  println(Monoid(BooleanInstances.xor).combine(false, true))
+  println(Monoid(BooleanInstances.xor).combine(false, false))
+
+  //XNOR
+  println(Monoid(BooleanInstances.xnor).combine(true, true))
+  println(Monoid(BooleanInstances.xnor).combine(true, false))
+  println(Monoid(BooleanInstances.xnor).combine(false, true))
+  println(Monoid(BooleanInstances.xnor).combine(false, false))
+}
